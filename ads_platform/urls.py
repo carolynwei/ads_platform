098842dad31eb3350de5fdc304de65a5750f263e
carolynwei <carolynblack401@gmail.com>
2025-05-07 17:33:46 +0800

    后端功能完善一点

---
+++ ads_platform/urls.py
@@ -0,0 +1,11 @@
+# ad_platform/ad_platform/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/', include('ads.urls')), 
+    
+]
+
