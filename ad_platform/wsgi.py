f0d98aecede4bacaf3b2bbeae5452d4df9b390dc
carolynwei <carolynblack401@gmail.com>
2025-05-07 17:38:06 +0800

    删除了多余文件夹

--- ad_platform/wsgi.py
+++
@@ -1,6 +0,0 @@
-# ad_platform/ad_platform/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ad_platform.settings')
-application = get_wsgi_application()
